6203ae7e72ec344d7ae9a73cd4f1593a01d5ddf1
imados <imad.kh@outlook.com>
2016-07-13 23:58:22 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,112 @@
+
+
+  function initAds() {
+    if (admob) {
+      var adPublisherIds = {
+        ios : {
+          banner : "ca-app-pub-5231842333475288/1159723459"
+        },
+        android : {
+          banner : "ca-app-pub-5231842333475288/1159723459",
+          interstitial : "ca-app-pub-5231842333475288/4113189854",
+        }
+      };
+
+      var admobid = (/(android)/i.test(navigator.userAgent)) ? adPublisherIds.android : adPublisherIds.ios;
+
+      admob.setOptions({
+        publisherId:      admobid.banner,
+        interstitialAdId: admobid.interstitial,
+      });
+
+      registerAdEvents();
+
+    } else {
+      alert('AdMobAds plugin not ready');
+    }
+  }
+
+  function onAdLoaded(e) {
+    if (e.adType === admob.AD_TYPE.INTERSTITIAL) {
+      admob.showInterstitialAd();
+      showNextInterstitial = setTimeout(function() {
+        admob.requestInterstitialAd();
+      }, 40 * 1000); // 2 minutes
+    }
+  }
+
+  // optional, in case respond to events
+  function registerAdEvents() {
+    document.addEventListener(admob.events.onAdLoaded, onAdLoaded);
+    document.addEventListener(admob.events.onAdFailedToLoad, function (e) {});
+    document.addEventListener(admob.events.onAdOpened, function (e) {});
+    document.addEventListener(admob.events.onAdClosed, function (e) {});
+    document.addEventListener(admob.events.onAdLeftApplication, function (e) {});
+    document.addEventListener(admob.events.onInAppPurchaseRequested, function (e) {});
+  }
+
+  function onDeviceReady() {
+    document.removeEventListener('deviceready', onDeviceReady, false);
+    initAds();
+
+    // display a banner at startup
+    admob.createBannerView();
+
+    // request an interstitial
+    admob.requestInterstitialAd();
+  }
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function get_remote_action(){
+	$.ajax({
+	  url: "http://apps.coding-labs.com/getAction.php?app=com.coding_labs.Learing_Guitar_ar",
+	  context: document.body
+	}).done(function(data) {
+	  $("#remote").html(data);
+	  console.log(data)
+	});
+
+}
+//get_remote_action();
+//////////////::
+
+$(".home").click(function(){
+	document.location = "../main.html";
+});
+$("#menu__").click(function(){
+	$(".details").hide();
+	$(".main_cnt").show();
+	window.scrollTo(0, 0);
+});
+$("#next").click(function(){
+	id = $(".details:visible").attr("id").replace("p","");
+	if($(".details").length ==id) return;
+	console.log(id);
+	$(".details").hide();
+	$("#p"+(parseInt(id)+1)).show();
+	window.scrollTo(0, 0);
+});
+$("#prev").click(function(){
+	id = $(".details:visible").attr("id").replace("p","");
+	console.log(id);
+	if(id ==1) return;
+	$(".details").hide();
+	$("#p"+(parseInt(id)-1)).show();
+	window.scrollTo(0, 0);
+});
+
+$(".options").click(function(){
+	document.location = "Guides/0.html";
+});
+    
+$(".Guides").click(function(){
+	id = $(this).attr("data_id");
+	url = "Guides/"+id+".html";
+	//document.location="#p"+id;
+	$(".details").hide();
+	$(".main_cnt").hide();
+	$("#p"+id).show();
+});
+$(document).ready(function(){
+	
+});
